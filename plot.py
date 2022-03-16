b74015c9e360a5a85522231f6346a6847a35f5d2
jurikjir <jirka.jurik@gmail.com>
2022-03-16 01:39:14 +0100

    improve plotter, create func for main

--- plot.py
+++ plot.py
@@ -3,6 +3,10 @@ import seaborn as sns
 import pandas as pd
 from typing import Callable, Tuple
 import os
+from tqdm import tqdm
+
+from data_loader import load_data
+from preprocess import PreprocessData
 
 
 class PlotData(object):
@@ -13,16 +17,18 @@ class PlotData(object):
         self.plot_storage = os.path.join(imgdir_path, "img")
         if not os.path.exists(self.plot_storage):
             os.makedirs(self.plot_storage)
+        self.plot_paths = []
 
     def plot(
         self,
         data: pd.DataFrame,
-        x: str,
-        y: str,
         plot: Callable,
+        x: str,
+        y: str = None,
         rotation: int = 0,
         figsize: Tuple[int]=(8, 6),
         autosave: bool = False,
+        grid: bool = False,
         **kwargs) -> None:
         """
         Function which takes data, x, y columns and sns or plt function and plot
@@ -30,16 +36,22 @@ class PlotData(object):
         plotting.
         """
         plt.figure(figsize=figsize)
-        ax = plot(data[x], data[y], **kwargs)
+        
+        ax = plot(data[x], data[y], **kwargs) if y is not None else plot(data[x], **kwargs)
         ax.set_xticklabels(ax.get_xticklabels(), rotation=rotation)
         plot_name = str(plot).split(" ")[1]
         if "str" in x: x = x.split("_")[0]
-        if "str" in y: y = y.split("_")[0]
-        plt.title(label=f"{x} vs {y} - {plot_name}", fontweight="bold")
         plt.xlabel(xlabel=x, fontweight="bold")
-        plt.ylabel(ylabel=y, fontweight="bold")
-        #plt.legend()
+        if y is not None:
+            if "str" in y: y = y.split("_")[0]
+            plt.ylabel(ylabel=y, fontweight="bold")
+            plt.title(label=f"{x} vs {y} - {plot_name}", fontweight="bold")
+        else:
+            plt.title(label=f"{x} - {plot_name}", fontweight="bold")
+        if grid:
+            plt.grid()
         save_path = os.path.join(self.plot_storage, f"{x}_{y}_{plot_name}.png")
+        self.plot_paths.append(save_path)
         plt.savefig(save_path)
         if autosave == False:
             plt.show()
@@ -49,4 +61,43 @@ class PlotData(object):
             if save_img.lower() in ["n", "no", "nope"]:
                 os.remove(path=save_path)
         elif autosave == True:
-            print(f"Plot saved as {save_path}")
+            pass
+
+    @property
+    def get_plot_paths(self) -> list:
+        """
+        Function which returns list of paths to saved plots
+        """
+        return self.plot_paths
+
+
+def plot_data(processed_data: pd.DataFrame) -> None:
+    """
+    Plots data and saves them into img folder
+    """
+    levels = ['E_level', 'N_level', 'A_level', 'C_level', 'O_level']
+    trait_order = ["ext_low", "low", "lower", "neutral", "higher", "high", "ext_high"]
+    age_order = ["14-20", "20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90", "90-100"]
+    plots = [sns.barplot, sns.boxplot, sns.violinplot]
+    plotter = PlotData(imgdir_path="results")
+
+    for plot in tqdm(plots):
+        for level in levels:
+            plotter.plot(
+                data=processed_data,
+                x=level,
+                y="performance",
+                plot=plot,
+                order=trait_order,
+                autosave=True)
+
+        plotter.plot(
+            data=processed_data,
+            x="age_range",
+            y="performance",
+            plot=plot,
+            order=age_order,
+            autosave=True)
+
+    for plot_path in plotter.get_plot_paths:
+        print(f"Plot saved to {plot_path}")
