6cb05fcaacd602782619e0f466367d65480b2148
jurikjir <jirka.jurik@gmail.com>
2022-03-16 10:44:42 +0100

    add numpy

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from tqdm import tqdm
 import pandas as pd
+import numpy as np
 
 from data_loader import load_data
 from plot import plot_data
